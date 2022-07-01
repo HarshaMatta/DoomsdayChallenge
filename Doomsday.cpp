edf6178cb757fc3f32bbe053682316e8f2fc557a
DominicLo <dominic.lo@live.co.uk>
2022-07-01 14:41:26 +0100

    the beginning

---
+++ Doomsday.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dominic on 01/07/2022.
+//
+
+#include "Doomsday.h"
---
+++ Doomsday.h
@@ -0,0 +1,14 @@
+//
+// Created by Dominic on 01/07/2022.
+//
+
+#ifndef DOOMSDAY_ALGORITHM_DOOMSDAY_H
+#define DOOMSDAY_ALGORITHM_DOOMSDAY_H
+
+
+class Doomsday {
+
+};
+
+
+#endif //DOOMSDAY_ALGORITHM_DOOMSDAY_H
